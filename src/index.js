6a50f0f9a7645fa46acb29070df96408126059a6
Dmytro Slipchenko <dslipchenko@gmail.com>
2024-08-16 00:33:03 +0300

    feat: add slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-// const swiper = new Swiper(...)
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	loop: true,
+	spaceBetween: 100,
+	slidesPerView: 1,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
